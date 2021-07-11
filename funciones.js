0a2c0292b9bf8e90d8887af8b387b53cba580aba
sabw14 <sabw14@gmail.com>
2021-07-11 23:13:42 +0000

    funciones de saludo y suma

--- funciones.js
+++ funciones.js
@@ -13,3 +13,17 @@ var miFuncion = function(a, b) {
 }
 
 miFuncion();
+
+
+//Saludo estudiantes
+
+function saludarEstuadiantes(estudiante) {
+    console.log(`Hola ${estudiante}`);
+}
+
+//Obtener resultados y no usar el console log
+
+function sumar(a,b) {
+    var resultado = a + b;
+    return resultado;    
+}
